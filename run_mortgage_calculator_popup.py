2ad547522411343b63cd261254556391652c3fae
Doggod003 <severett005@gmail.com>
2025-07-23 10:40:36 -0400

    Update run_mortgage_calculator_popup.py

--- run_mortgage_calculator_popup.py
+++ run_mortgage_calculator_popup.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Mortgage Calculator", layout="centered")
 st.title("🏡 Mortgage Calculator with PMI")
