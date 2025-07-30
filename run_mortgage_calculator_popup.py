5f5807faf0dd65e9b7381508d1a31b8b04531476
Doggod003 <severett005@gmail.com>
2025-07-30 10:29:27 -0400

    Update run_mortgage_calculator_popup.py

--- run_mortgage_calculator_popup.py
+++ run_mortgage_calculator_popup.py
@@ -4,8 +4,8 @@ import streamlit as st
 import pandas as pd
 
 
-def load_local_css(assets/tabs.css):
-    with open(assets/tabs.css) as f:
+def load_local_css(file_path):
+    with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Load the CSS at the beginning of the app
