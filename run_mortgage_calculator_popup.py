abf2ebbe567fa722154b18fab8b3c81358c04ddc
Doggod003 <severett005@gmail.com>
2025-07-31 12:06:59 -0400

    Update run_mortgage_calculator_popup.py

--- run_mortgage_calculator_popup.py
+++ run_mortgage_calculator_popup.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 from buttons import reset_year_filter
-from st_aggrid import AgGrid, GridOptionBuilder
+from st_aggrid import AgGrid, GridOptionsBuilder
 
 
 st.set_page_config(page_title="Mortgage Calculator", layout="wide")
