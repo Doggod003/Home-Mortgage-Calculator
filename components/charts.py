4ce29a74392651b7770aa3a757ee11a24cb83a2a
Doggod003 <severett005@gmail.com>
2025-07-31 11:48:11 -0400

    Create charts.py

---
+++ components/charts.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import plotly.graph_objects as go
+
+def draw_balance_chart(filtered_df):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=filtered_df["Month"],
+        y=filtered_df["Balance"],
+        mode='lines+markers',
+        name='Balance',
+        line=dict(color='blue')
+    ))
+    fig.update_layout(
+        xaxis_title="Month",
+        yaxis_title="Balance ($)",
+        template="plotly_white",
+        legend=dict(x=1.05, y=1),
+        margin=dict(r=120)
+    )
+    st.plotly_chart(fig, use_container_width=True)
